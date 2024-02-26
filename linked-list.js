9c30523997afa392111ecb52d1962768c678534f
CyberMermaid <45833464+CyberMermaid@users.noreply.github.com>
2024-02-25 21:57:42 -0500

    Done

--- linked-list.js
+++ linked-list.js
@@ -38,22 +38,25 @@ class LinkedList {
     if(this.head === null){
       this.head = newNode;
       return this.head;
-    } else{
+    } 
+    else{
       newNode.next = this.head;
       this.head = newNode;
       return newNode;
     }
   }
 
+  // javascript Throws error if list is empty.
   /** pop(): return & remove last item. */
 
   pop() {
+    // If the list is empty
     if(!this.head){
       this.head = null
       this.tail = null
       return null;
     }
-    // if only one node in the list
+    // if only one node is in the list
     else if(!this.head.next){
       let lastNode = this.head;
       this.head = null;
@@ -94,33 +97,128 @@ class LinkedList {
   }
 
   /** getAt(idx): get val at idx. */
-
   getAt(idx) {
+    // Check if the linked list is empty
+    if(!this.head){
+      return null;
+    }
 
+    let currentNode = this.head;
+    let count = 0;
+      
+      // Traverse linked list until the node at the idx has been reached
+    while(currentNode!==null && count<= idx){
+    // When the index has been reached, return the value of the node at the specified index
+        if (count === idx) {
+              return currentNode.value;
+            }    
+        //increment counter and update currentNode 
+        count++;
+        currentNode = currentNode.next; 
+  }
+    return null;
   }
 
   /** setAt(idx, val): set val at idx to val */
-
   setAt(idx, val) {
-
+    if (idx <0 || idx >= this.length) { 
+      throw new Error("Index out of bounds!");
+     }
+    let currentNode = this.head;
+    for(let i=0;i< idx; i++){
+      currentNode= currentNode.next;
+    }
+    currentNode.value = val;
+    return currentNode.value;
   }
 
   /** insertAt(idx, val): add node w/val before idx. */
-
   insertAt(idx, val) {
+    if (idx <0 || idx > this.length) { 
+      throw new Error("invalid index!");
+     }
+    
+    // Create a node with the given value
+    let addedNode = new Node(val);
 
+    // Check if Linked list is empty or if you're inserting at the head.
+    if (!this.head || idx ===0 ){
+      addedNode.next = this.head;
+      this.head = addedNode;
+      // Increment length of linked list
+      this.length++;
+      return null; 
+    }
+
+   // traverse to the node before desired insertion point
+    let previousNode = this.head;
+    for(let i = 0; i<idx; i++){
+      previousNode = previousNode.next;
+      }
+    addedNode.next = previousNode.next;
+    previousNode.next = addedNode;
+    this.length++;
   }
 
   /** removeAt(idx): return & remove item at idx, */
+  removeAt(idx) { 
 
-  removeAt(idx) {
+    // First check if index is valid
+    if (idx <0 || idx > this.length) { 
+      throw "invalid index!"; }
 
+    // Check if Linked list is empty
+    if (!this.head){
+      return null;
+    }
+
+    // Remove headnode if index is 0.
+    if (idx === 0){
+      let removedNode = this.head;
+      this.head = this.head.next;
+      // Update the length of the list
+      this.length--; 
+      return removedNode;
+    }
+
+    let currentNode = this.head;
+    
+    // traverse through linked list before node at index
+    for(let i = 0; i<idx; i++){
+      currentNode = currentNode.next;
+    }
+      
+    // Ensure that the node to be removed exists
+    if (!currentNode.next) {
+      throw "Node does not exist!";
+    }
+    // Update removedNode
+    removedNode = currentNode.next;      
+    // Unlink the deleted node from list
+    currentNode.next = currentNode.next.next;
+    // Update the length of the list
+    this.length --; 
+    return removedNode;
   }
 
   /** average(): return an average of all values in the list */
-
   average() {
+    let count=0 , sum = 0;
     
+     // If the list is empty
+     if(!this.head){
+        return null;
+     }
+     let currentNode= this.head;
+     //Traverse through linked list
+     while(currentNode !== null){
+        sum += currentNode.value;
+        count++;
+        currentNode = currentNode.next;
+      }
+      // Calculate and return average
+      let avg = sum/count;
+      return Number(avg.toPrecision(4));
   }
 }
 
